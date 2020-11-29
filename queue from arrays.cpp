4dff28f2e8dc51975e5ea633165f27600a28b866
Nischay19 <75144765+Nischay19@users.noreply.github.com>
2020-11-30 00:32:11 +0530

    Update queue from arrays.cpp

--- queue from arrays.cpp
+++ queue from arrays.cpp
@@ -1,18 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int queue[50], n = 150, front = - 1, rear = - 1;
+int queue1[50], n = 150, front = - 1, rear = - 1;
 
-  void enqueue(int value) {
+  void enqueue() {
    if (rear == n - 1)
       cout<<"Queue Overflow"<<endl;
    else {
       if (front == - 1)
       front = 0;
-      cout<<"Insert the element in queue : "<<endl;
+      int value;
+      cout<<"Insert the element in queue1 : "<<endl;
       cin>>value;
       rear++;
-      queue[rear] = value;
+      queue1[rear] = value;
    }
 }
   void dequeue() {
@@ -20,7 +21,7 @@ int queue[50], n = 150, front = - 1, rear = - 1;
       cout<<"Queue Underflow ";
       return ;
    } else {
-      cout<<"Element deleted from queue is : "<< queue[front] <<endl;
+      cout<<"Element deleted from queue is : "<< queue1[front] <<endl;
       front++;;
    }
 }
@@ -30,19 +31,19 @@ int queue[50], n = 150, front = - 1, rear = - 1;
    else {
       cout<<"Queue elements present are : ";
       for (int i = front; i <= rear; i++)
-      cout<<queue[i]<<" "<<endl;
+      cout<<queue1[i]<<" "<<endl;
          //cout<<endl;
    }
 }
 int main() {
    int ch;
-   cout<<"1. Insert element to queue"<<endl;
-   cout<<"2. Delete element from queue"<<endl;
-   cout<<"3. Display all the elements of the queue"<<endl;
+   cout<<"1. Insert element to queue1"<<endl;
+   cout<<"2. Delete element from queue1"<<endl;
+   cout<<"3. Display all the elements of the queue1"<<endl;
    cout<<"4. Exit"<<endl;
    do {
       cout<<"Enter your choice : "<<endl;
-      cin<<ch;
+      cin>>ch;
       if(ch==1){
            enqueue();
                }
